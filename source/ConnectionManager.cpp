e27d3b615b3ec3e2c93ea267c9249feb73c15d91
Sara Adra <Adra001@users.noreply.github.com>
2016-03-15 09:16:10 -0700

    Added files via upload

---
+++ source/ConnectionManager.cpp
@@ -0,0 +1,31 @@
+// Libraries
+#include <iostream>
+#include <string>
+
+// Included Header Files
+#include "ConnectionManager.h"
+
+using namespace std;
+
+/*
+Function: New Connection
+Input: list of strings 
+Purpose:  creates a new connection between user and db
+*/
+bool NewConnection(List[])
+{
+	return true;
+}
+
+
+/*
+Function: Delete Connection
+Input: deletes a connection
+Purpose: disconnects user from db
+*/
+
+bool DelConnection(connection)
+{
+
+return true; 
+}
---
+++ source/ConnectionManager.h
@@ -0,0 +1,42 @@
+#ifndef CONNECTION_MANAGER_H
+#define CONNECTION_MANAGER_H
+
+// Libraries
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* Included Header Files */
+#include "Connection.h"
+#include "ConnectionManager"
+
+
+class ConnectionManager
+{
+protected:
+	List Connections; // array?
+	List hashIdentifier; //??
+	List numofConnectionUsers; //
+	
+public:
+	newConnection(List<string>); //:connection
+	deeteConnection(connection);
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
