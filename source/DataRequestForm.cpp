0e17ae6f64166e03790670e00b6491c9d0ca66a7
jordanspinato <spina004@cougars.csusm.edu>
2016-03-27 18:06:40 -0700

    Implementation for getTable

--- source/DataRequestForm.cpp
+++ source/DataRequestForm.cpp
@@ -18,7 +18,19 @@ Purpose: calls frunction create user from userAccount class
 */
 bool getTable(string table)
 {
+    // if connection exists return table
+  if () 
 
+  // connection doesnt exist.. create new connection
+  else {
+    connMgr.newConnection(conn);
+    conn.getTableContents(table);
+
+  }
+
+
+  // delete connection 
+  connMgr.deleteConnection(conn);
 
 
 	return true;
