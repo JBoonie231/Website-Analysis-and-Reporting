6412d876489180ee91be20d26ba3f3b51f84ff6d
jordanspinato <spina004@cougars.csusm.edu>
2016-03-28 22:47:52 -0700

    changed getTable function

--- source/DataRequestForm.cpp
+++ source/DataRequestForm.cpp
@@ -19,15 +19,23 @@ Purpose: calls frunction create user from userAccount class
 bool getTable(string table)
 {
   string[][] contents;
+  bool connectionState = false;
 
-    // if connection exists return table
-  if () 
+  // if connection exists return table
+  if ()
+  {
+    contents = connection.getTableContents(table);
+    table = &contents;
+    connectionState = true;
+
+  }
 
   // connection doesnt exist.. create new connection
   else {
     connMgr.newConnection(conn);
     contents = conn.getTableContents(table);
     table = &contents; // point to address of the contents table
+    connectionState = true;
   }
 
 
@@ -35,7 +43,7 @@ bool getTable(string table)
   connMgr.deleteConnection(conn);
 
 
-	return true;
+	return connectionState;
 
 }
 
