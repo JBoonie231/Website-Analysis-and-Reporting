09d98f051bd1361545f6156a827fdbe3df660c7e
Joshua Boone <Jboonie231@yahoo.com>
2016-04-07 10:14:22 -0700

    Update the object declaration in push_back call.

--- source/ConnectionManager.cpp
+++ source/ConnectionManager.cpp
@@ -40,7 +40,7 @@ Connection ConnectionManager::newConnection(vector<string> identifiers)
 	}
 
 	// If connection wasn't found, create connection and connection values.
-	connections.push_back(new Connection(identifiers));
+	connections.push_back(Connection temp_conn(identifiers));
 	hashIdentifiers.push_back(hashIdentifier);
 	numOfConnectionUsers.push_back(1);
 
